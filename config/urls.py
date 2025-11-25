23dffad010109de1905daff7678c6f5308a0df4f
Spiderus Spiderus <carli@integrumpr.com>
2025-11-24 22:57:47 -0400

    feat(api): add hello world endpoint to verify DRF installation

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from tasks.views import health, hello_world
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("api/health/", health),
+    path("api/hello/", hello_world),
+    ]
